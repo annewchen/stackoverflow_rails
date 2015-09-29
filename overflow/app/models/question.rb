0048d22a5ef730a6487f8dbf4a57352b0dbc5357
Anne Chen <annewchen@gmail.com>
2015-09-29 15:30:16 -0700

    add user resource, without login/sessions

---
+++ overflow/app/models/question.rb
@@ -0,0 +1,6 @@
+class Question < ActiveRecord::Base
+  belongs_to :user
+
+  validates :title, :description, presence: true
+
+end
---
+++ overflow/app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  has_many :questions
+
+  validates :username, :password, presence: true
+end
