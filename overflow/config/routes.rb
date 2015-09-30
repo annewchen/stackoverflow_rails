6d2f176dae828641d9c25eba68f295f9d7f39f8d
Anne Chen <annewchen@gmail.com>
2015-09-29 19:43:00 -0700

    add resource questions, incomplete functionality

--- overflow/config/routes.rb
+++ overflow/config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :users, only: [:new, :create, :show, :edit, :update] do
-    resources :questions
+  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :questions do
+    resources :answers
   end
+
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
