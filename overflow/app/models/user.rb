b2945b78f2eb20e19b0d67c5e29e1d077161fe5e
Anne Chen <annewchen@gmail.com>
2015-09-30 21:29:45 -0700

    add destroy dependencies on question and user model

--- overflow/app/models/user.rb
+++ overflow/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :questions
-  has_many :answers
+  has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
